ff8b42ac2005f5334e2e22b5b3a5c515c9991b2b
morkar <morkar@gmx.de>
2015-05-01 16:11:34 +0200

    Added classes for enemies of different size

---
+++ source/SmallEnemyShip.hx
@@ -0,0 +1,34 @@
+package ;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColorUtil;
+import flixel.util.FlxPoint;
+import flixel.util.FlxRandom;
+import flixel.util.FlxVector;
+
+/**
+ * ...
+ * @author 
+ */
+class SmallEnemyShip extends EnemyShip
+{
+	private function new() 
+	{
+		super();
+		this.makeGraphic(20, 20, FlxColorUtil.makeFromARGB(1, 200, 20, 20));
+		this.origin.set();
+		this.scale.set(GameProperties.GetScaleFactor(), GameProperties.GetScaleFactor());
+	}
+	
+	public static function spawn (pos: FlxPoint, velX  : Float = 5, velY  : Float = 0 , dir   :Int = 1 ) : EnemyShip
+	{
+		var e :EnemyShip = new SmallEnemyShip();
+		e.setPosition(pos.x, pos.y);
+		e.velocity.set(velX * dir, velY * dir);
+		
+		return e;
+	}	
+}
