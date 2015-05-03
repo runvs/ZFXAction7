b0126949936edff10dbb90c13f848c006fb51fda
Laguna <laguna_999@web.de>
2015-05-03 18:18:43 +0200

    adding projectile glow

--- source/SmallSpaceShipGunProjectile.hx
+++ source/SmallSpaceShipGunProjectile.hx
@@ -7,6 +7,7 @@ import flixel.tweens.FlxTween;
 import flixel.util.FlxColorUtil;
 import flixel.util.FlxPoint;
 import flixel.util.FlxRandom;
+import flixel.util.FlxTimer;
 import flixel.util.FlxVector;
 
 /**
@@ -15,20 +16,36 @@ import flixel.util.FlxVector;
  */
 class SmallSpaceShipGunProjectile extends Projectile
 {
+	
+	var _glow : FlxSprite;
+	
 	public function new(x: Float, y: Float) 
 	{
 		super();
-		this.makeGraphic(6, 6, FlxColorUtil.makeFromARGB(1, 200, 20, 20));
-		//this.origin.set();
+		this.makeGraphic(4, 4, FlxColorUtil.makeFromARGB(1, 200, 20, 20));
 		this.acceleration.set(0, 25);
 		this.velocity = new FlxPoint(0, 50);
 		this.x = x;
 		this.y = y;
 		this.maxVelocity.set(75, 100);
+		
+		_glow = new FlxSprite();
+		_glow.loadGraphic(AssetPaths.projectileEnemyGlow__png, false, 12, 12);
+		_glow.scale.set(2, 2);
+		_glow.alpha = 0.25;
+		_glow.offset.set(4, 4);
+		FlxTween.tween(_glow, { alpha:0.5 }, 2.3, { type:FlxTween.PINGPONG, ease:FlxEase.sineInOut } );
 	}	
 	
 	public override function update() : Void 
 	{
 		super.update();
+		_glow.setPosition(x, y);
+	}
+	
+	public override function draw() : Void 
+	{
+		_glow.draw();
+		super.draw();
 	}
 }
